fbb00479928292500594ed8074eb2a338a61de37
Trygve-Poppe <78844928+Trygve-Poppe@users.noreply.github.com>
2021-03-24 10:58:27 +0100

    T

--- settings.py
+++ settings.py
@@ -1,10 +1,19 @@
 LOSS = "mse"
 OPTIMIZER = "adam"
 VALIDATION_SPLIT = 0.2
-PREDICTION_SPLIT = 0.01
+PREDICTION_SPLIT = 0.02
 NN_HIDDEN_LAYERS = (32, 64, 32)
 INJECTION_LAYERS = {1: (1, )}
 ACTIVATION = 'relu'
+<<<<<<< Updated upstream
+=======
+
+
+"""
+Training data
+"""
+DATA_FILE = 'LargeLV.tsv'
+>>>>>>> Stashed changes
 N = 400000  # (Maximum) Training data size
 SPARSE = 1  # Number of timesteps to skip when creating training data
 PREDICTION_TIME_STEP = 0.05
